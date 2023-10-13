df8c8dc6029089341d55cc26bb9c17267ba14f03
SunboyTPB <107842414+SunboyTPB@users.noreply.github.com>
2023-10-13 19:51:00 +0800

    master

--- json_format_js/writings.js
+++ json_format_js/writings.js
@@ -4,13 +4,13 @@ var writings = [
         "title": "东方双世录",
         "type": "同人小说",
         "description": "世界毁灭之际，三个少年少女在「生命的最后一刻」来到了陌生的世界。在这里，他们遇见了新的同伴，也遇见了他们共同的强敌。为了守护这陌生的世界，也为了拯救自己的世界，三个少年少女开始和同伴们互相合作……",
-        "novelurl": "SF轻小说：https://book.sfacg.com/Novel/606519/"
+        "novelurl": "SF轻小说：https://book.sfacg.com/Novel/606519/\r\n哔哩哔哩专栏：https://www.bilibili.com/read/cv22536882\r\n晋江文学：https://www.jjwxc.net/onebook.php?novelid=8191174&chapterid=1"
     },
     {
         "id": 2,
         "title": "Fate; Two World",
         "type": "同人小说",
         "description": "第五次圣杯战争爆发二十年以后的2024年，一个大圣杯却突然出现了。而这个大圣杯也牵扯到另外一个世界。人们把这种规模的圣杯战争，称之为圣杯讨伐战。而现在，两个世界的他们将会做出不同的选择。这两个世界的命运将会走向何处？",
-        "novelurl": "敬请期待"
+        "novelurl": "SF轻小说：https://book.sfacg.com/Novel/650124/\r\n哔哩哔哩专栏：https://www.bilibili.com/read/cv24652183?spm_id_from=333.999.0.0"
     },
 ]
